91f70dade1da5af94075e6603d367b845616233d
CRPVilaca <142179946+CRPVilaca@users.noreply.github.com>
2023-08-15 17:34:32 +0100

    Add table_generator.sql

---
+++ table_generator.sql
@@ -0,0 +1,41 @@
+CREATE TABLE movie (
+movie_id INT PRIMARY KEY,
+title VARCHAR(50),
+year INT,
+genre VARCHAR(50),
+country VARCHAR(50),
+directors VARCHAR(50),
+rating DECIMAL(1,1),
+duration INT);
+
+
+INSERT INTO movie VALUES(1, 'After-Depois da Verdade', 2020, 'Drama, Romance', 'United States', 'Roger Kumble', 5.0, 105);
+INSERT INTO movie VALUES(2, 'Sonic the Hedgehog', 2020, 'Action, Adventure, Comedy', 'United States, Japan, Canada','Jeff Fowler', 6.5, 99);
+INSERT INTO movie VALUES(3, 'Star Wars: Episode IX- The Rise of Skywalker', 2019, 'Action, Adventure, Fantasy, Sci-Fi','United States', 'J.J. Abrams', 6.5, 141);
+INSERT INTO movie VALUES(4, 'Everything Everywhere All at Once', 2022, 'Action, Adventure, Comedy, Fantasy, Sci-Fi', 'United States', 'Daniel Kwan, Daniel Scheinert', 7.8, 139);
+INSERT INTO movie VALUES(5, 'Top Gun: Maverick', 2022,'Action, Drama', 'United States', 'Joseph Kosinski', 8.3, 130);
+INSERT INTO movie VALUES(6, 'Avengers: Endgame', 2019, 'Action, Adventure, Drama, Sci-Fi', 'United States','Anthony Russo, Joe Russo', 8.4, 181);
+INSERT INTO movie VALUES(7, 'Joker', 2019, 'Crime, Drama, Thriller', 'United States, Canada', 'Todd Phillips', 8.4, 122);
+INSERT INTO movie VALUES(8, 'Parasite', 2019, 'Drama, Thriller', 'South Korea', 'Bong Joon Ho', 8.5, 132);
+INSERT INTO movie VALUES(9, 'Avatar: The Way of Water', 2022,'Action, Adventure, Fantasy, Sci-Fi', 'United States', 'James Cameron', 7.6, 192);
+INSERT INTO movie VALUES(10, 'Spider-Man: No Way Home', 2021, 'Action, Adventure, Fantasy, Sci-Fi', 'United States', 'Jon Watts', 8.2, 148);
+INSERT INTO movie VALUES(11, 'CODA', 2021, 'Comedy, Drama, Music', 'France, Canada, United States', 'Sian Heder', 8.0, 111);
+INSERT INTO movie VALUES(12, 'Megan', 2020, 'Horror, Thriller', 'Italy', 'Silvio Nacucchi', 3.9, 89);
+INSERT INTO movie VALUES(13, 'Dark Phoenix', 2010, 'Action, Adventure, Sci-Fi', 'United States, Canada','Simon Kinberg', 5.7, 113);
+INSERT INTO movie VALUES(14, 'Avengers: Infinity War', 2018, 'Action, Adventure, Sci-Fi', 'United States', 'Anthony Russo, Joe Russo', 8.4, 149);
+INSERT INTO movie VALUES(15, 'Frozen II', 2019, 'Action, Adventure, Comedy', 'United States, Canada', 'David F. Sandberg', 7.0, 132);
+INSERT INTO movie VALUES(16, 'Venom', 2018, 'Action, Adventure, Sci-Fi', 'United States, China', 'Ruben Fleischer', 6.6, 112);
+INSERT INTO movie VALUES(17, 'The Nun', 2018,'Horror, Mystery, Thriller', 'United States', 'Corin Hardy', 5.3, 96);
+INSERT INTO movie VALUES(18, 'Run Rabbit Run', 2023, 'Horror, Thriller', 'Australia', 'Daina Reid', 5.0, 100);
+INSERT INTO movie VALUES(19, 'Toy Story 4', 2019, 'Animation, Adventure, Comedy', 'United States', 'Josh Cooley', 7.7, 100);
+INSERT INTO movie VALUES(20, 'Scream VI', 2023, 'Horror, Mystery, Thriller', 'United States, Canada', 'Matt Bettinelli-Olpin, Tyler Gillett', 6.6, 122);
+INSERT INTO movie VALUES(21, 'A través del mar', 2023, 'Comedy, Drama, Romance','Spain', 'Marçal Forés', 5.0, 109);
+INSERT INTO movie VALUES(22, 'Green Book', 2018, 'Biography, Comedy, Drama', 'United States, China', 'Peter Farrelly', 8.2, 130);
+INSERT INTO movie VALUES(23, 'The Fabelmans', 2022, 'Drama', 'United States, India', 'Steven Spielberg', 7.6, 151);
+INSERT INTO movie VALUES(24, 'The Suicide Squad', 2021, 'Action, Adventure, Comedy', 'United States','James Gunn', 7.2, 132);
+INSERT INTO movie VALUES(25, 'Indiana Jones and the Dial of Destiny', 2023, 'Action, Adventure', 'United States', 'James Mangold', 6.9, 154);
+INSERT INTO movie VALUES(26, 'Mulan', 2020, 'Action, Adventure, Drama', 'United States, China', 'Niki Caro', 5.8, 115);
+INSERT INTO movie VALUES(27, 'Spider-Man: Far from Home', 2019, 'Action, Adventure, Comedy', 'United States', 'Jon Watts', 7.4, 129);
+INSERT INTO movie VALUES(28, 'The Lion King', 2019, 'Animation, Adventure, Drama', 'United States, United Kingdom', 'Jon Favreau', 6.8, 118);
+INSERT INTO movie VALUES(29, 'Stowaway', 2021, 'Adventure, Sci-Fi, Thriller', 'United Kingdom, Germany, United States', 'Joe Penna', 5.6, 116);
+INSERT INTO movie VALUES(30, 'Elemental', 2023, 'Animation, Adventure, Comedy', 'United States', 'Peter Sohn', 7.1, 101);
